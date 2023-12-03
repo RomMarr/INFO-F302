a74433a987be86b3909c7f6823cee600d227b7a8
RomMarr <romain.markowitch@gmail.com>
2023-12-03 19:25:20 +0100

    continuation Q1/2

--- src/project.py
+++ src/project.py
@@ -8,10 +8,10 @@ from automata.fa.nfa import NFA
 from pysat.solvers import Minisat22, Minicard
 from pysat.formula import CNF, CNFPlus, IDPool
 
+X(i[0,k],a[0,1] -> a=1 appartient à F,l[lettre aplhabet],j[0,k])
 
 # Variables 
-vpool1 = IDPool(start_from=1) 
-vpool2 = IDPool(start_from=2) # ???????????
+vpool = IDPool(start_from=1) 
 cnf = CNF()  # construction d'un objet formule en forme normale conjonctive (Conjunctive Normal Form)
 
 
@@ -20,8 +20,42 @@ def gen_aut(alphabet: str, pos: list[str], neg: list[str], k: int) -> DFA:
     # pos : P (ensemble des mots acceptant)
     # neg : N (ensemble des mots non-acceptant)
     
+    
+    # Un etat ne peux pas être acceptant et non acceptant en même temps
+    for etatI in range(k):
+        for lettre in alphabet:
+            for  etatF in range(k):
+                    cnf.append([-vpool.id(etatI,0,lettre,etatF), -vpool.id(etatI,1,lettre,etatF)])
+    
     # Commence à P0
-    pass
+    
+    for a in range(2) :
+        for mot in pos:
+            for etatF in range(k) :
+                cnf.append([vpool.id(0,a,mot[0],etatF)])
+
+    # Pas deux transitions possible
+        for etatI in range(k) :
+            for lettre in alphabet :
+                for a in range(2) :
+                    for etatF1 in range(k) :
+                        for etatF2 in range(k) :
+                            cnf.append(-[vpool.id(etatI,a,lettre,etatF1),-vpool.id(etatI,a,lettre,etatF2)])
+
+    # Tout mots de pos finit par un etat acceptant
+        for mot in pos :
+            for lettre in range(len(mot)-2):
+                for etatI1 in range(k) :
+                    for etatF in range(k) :
+                        for a in range(2) :
+                            for etatI2 in range(k) :
+                                cnf.append(-[vpool.id(etatI1,a,mot[lettre+1],etatF)], - [vpool.id(etatI2,a,mot[lettre],etatI)])
+
+                    
+                
+
+
+    return "Oui"
 
 # Q3
 def gen_minaut(alphabet: str, pos: list[str], neg: list[str]) -> DFA:
