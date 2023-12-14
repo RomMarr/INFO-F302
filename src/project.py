6c345cf22f244f7aa7243191d77e8050f166996d
Nvekemans <noe.vek@gmail.com>
2023-12-14 20:16:30 +0100

    passe_tout_sauf_7

--- src/project.py
+++ src/project.py
@@ -25,7 +25,7 @@ class Automate:
         self.FPool = IDPool(start_from=k+1) 
         self.transiPool = IDPool(start_from=2*k+1) 
         self.execPool = IDPool(start_from=2*k+(k**2)*len(alphabet)+1) 
-        self.etatFinal = IDPool(start_from= 10000000)
+        
         if ell == -1 :
             self.cnf = CNF()  # construction d'un objet formule en forme normale conjonctive (Conjunctive Normal Form)
         else:
@@ -36,7 +36,6 @@ class Automate:
         self.k = k
         self.ell = ell
         self.interpretation_filtree = None
-
 
     # Fonctions de création des clauses :
     def existance_etat_initial(self):
@@ -99,7 +98,7 @@ class Automate:
         # Lien entre chemin et transition :
         for x in range(self.k):  # x = transition initiale
             for y in range(self.k):  # y  = transition finale
-                for mot in (self.pos + self.neg):
+                for mot in (self.pos+self.neg):
                     for indiceLettre in range(len(mot)):
                         lettre = mot[indiceLettre]
                         self.cnf.append([-self.execPool.id((x, indiceLettre, mot)), -self.execPool.id((y, indiceLettre+1, mot)), self.transiPool.id((x,y,lettre))])
@@ -135,11 +134,11 @@ class Automate:
         self.cnf.append(d)
     
     def automate_complet(self):
-        for sommet_depart in range(self.k):
+        for x in range(self.k):
             for lettre in (self.alphabet) :
                 d=[]
-                for sommet_arive in range(self.k) :
-                    d.append(self.transiPool.id((sommet_depart,sommet_arive,lettre)))
+                for y in range(self.k) :
+                    d.append(self.transiPool.id((x,y,lettre)))
                 self.cnf.append(d)
 
     def automate_reversible(self):
@@ -149,7 +148,7 @@ class Automate:
                     for x2 in range(self.k) :  # sommet de départ 2
                         if x1 != x2 :
                             self.cnf.append([-self.transiPool.id((x1,y,lettre)), -self.transiPool.id((x2,y,lettre))])
-    
+
     def au_plus_ell_etats_acceptant(self):
         d = []
         for x in range(self.k) : # sommet           
@@ -163,20 +162,7 @@ class Automate:
             for x in range(self.k):
                 for y in range(self.k):
                     lettre = mot[-1]
-                    self.cnf.append(-self.execPool.id((x,len(mot)-1,mot)),-self.transiPool.id((x,y,lettre)),self.etatFinal.id((y,mot)))
-
-    def au_moins_une_exec_acceptante(self):
-        # Exécution finie sur état acceptant :
-        for mot in self.pos:
-            d = []
-            if mot != '' :
-                for x in range(self.k):  # x  = etat initiale
-                    d.append(-self.etatFinal.id((x,mot)))
-                    d.append(self.FPool.id((x)))                   
-            else :
-                d.append(self.FPool.id((0)))
-            self.cnf.append(d)    
-
+                    self.cnf.append(-self.execPool.id((x,len(mot)-1,mot)),-self.transiPool.id((x,y,lettre)),self.etatFinal.id((y,mot)))   
 
     def solve_aut(self):
         s = Minisat22(use_timer=True) # pour utiliser le solveur MiniSAT
@@ -200,6 +186,7 @@ class Automate:
             self.interpretation_filtree = list(filter(lambda x : x >=0, interpretation))
         self.interpretation_filtree
         return resultat
+    
 
 
     # Fonction de création d'automates (DFA et NFA) à partir des clauses : 
@@ -228,11 +215,54 @@ class Automate:
                     temp = transitions[sommetDepart]
                     temp[lettre] = sommetArive
                 transitions[sommetDepart] = temp
+        for sommet in states :
+            if sommet not in transitions:
+                transitions[sommet] = dict()
         print (states)
         print(transitions)
         print(accepting)
-        return DFA(states=states, input_symbols=set(self.alphabet), transitions =transitions, initial_state ="q0", final_states =accepting, allow_partial=True)
+        return DFA(states=states, input_symbols=set(self.alphabet), transitions =transitions, initial_state ="q0", final_states = accepting, allow_partial=True)
+    
+class AutomateNFA(Automate):
+    def __init__(self, alphabet: str, pos: list[str], neg: list[str], k: int, ell = -1):
+        somme = 0
+        for mot in pos:
+            somme += len(mot)*k
+        self.finalPool = IDPool(start_from= 1000)
+        super().__init__(alphabet,pos,neg,k,ell)
+        self.cnf = CNFPlus()
+    
+    def lien_etat_final_transition(self) :
+        for x in range(self.k):
+            for y in range(self.k):
+                for mot in self.pos:
+                    if mot != '':
+                        lettre = mot[-1]    # Dernière lettre du mot
+                        i = len(mot)-1  # Indice de la dernière lettre du mot
+                        self.cnf.append([-self.execPool.id((x,i,mot)),-self.transiPool.id((x,y,lettre)),-self.FPool.id((y)), self.finalPool.id((x,y,mot))])
+                        self.cnf.append([-self.finalPool.id((x,y,mot)),self.transiPool.id((x,y,lettre))])
+                        self.cnf.append([-self.finalPool.id((x,y,mot)),self.execPool.id((x,i,mot))])
+                    else:
+                        self.cnf.append([self.FPool.id((0))])
+                
 
+    def etat_final_implique_etat_acceptant (self) :
+        for x in range(self.k):
+            for mot in self.pos:
+                for y in range(self.k):
+                    self.cnf.append([-self.finalPool.id((x,y,mot)),self.FPool.id((y))])
+
+    
+    def au_moins_un_etat_acceptant(self):
+        for mot in self.pos:
+            if mot!='':
+                d = []
+                for x in range(self.k) : # sommet  
+                    for y in range(self.k) :         
+                        d.append(self.finalPool.id((x,y,mot)))
+                self.cnf.append(d)
+
+    
     def create_nfa(self, resultat):
         states = set()
         transitions = dict()
@@ -271,7 +301,6 @@ def gen_aut(alphabet: str, pos: list[str], neg: list[str], k: int) -> DFA:
     a.existance_etat_initial()  # existance état initial
     a.transition_valide()  # Transition valide  
     a.mot_commence_0()  # Tout mot commence à 0
-    a.lien_chemin_transition()  # Lien entre chemin et transition
     a.chemin_implique_transition()   # Chemin implique transition
     a.transition_implique_chemin()  # Transition implique chemin
     a.un_chemin_possible()  # Un seul chemin possible
@@ -291,7 +320,6 @@ def gen_minaut(alphabet: str, pos: list[str], neg: list[str]) -> DFA:
         a.existance_etat_initial()  # existance état initial
         a.transition_valide()  # Transition valide  
         a.mot_commence_0()  # Tout mot commence à 0
-        a.lien_chemin_transition()  # Lien entre chemin et transition
         a.chemin_implique_transition()   # Chemin implique transition
         a.transition_implique_chemin()  # Transition implique chemin
         a.un_chemin_possible()  # Un seul chemin possible
@@ -309,7 +337,6 @@ def gen_autc(alphabet: str, pos: list[str], neg: list[str], k: int) -> DFA:
     a.existance_etat_initial()  # existance état initial
     a.transition_valide()  # Transition valide  
     a.mot_commence_0()  # Tout mot commence à 0
-    a.lien_chemin_transition()  # Lien entre chemin et transition
     a.chemin_implique_transition()   # Chemin implique transition
     a.transition_implique_chemin()  # Transition implique chemin
     a.un_chemin_possible()  # Un seul chemin possible
@@ -325,7 +352,6 @@ def gen_autr(alphabet: str, pos: list[str], neg: list[str], k: int) -> DFA:
     a.existance_etat_initial()  # existance état initial
     a.transition_valide()  # Transition valide  
     a.mot_commence_0()  # Tout mot commence à 0
-    a.lien_chemin_transition()  # Lien entre chemin et transition
     a.chemin_implique_transition()   # Chemin implique transition
     a.transition_implique_chemin()  # Transition implique chemin
     a.un_chemin_possible()  # Un seul chemin possible
@@ -341,7 +367,6 @@ def gen_autcard(alphabet: str, pos: list[str], neg: list[str], k: int, ell: int)
     a.existance_etat_initial()  # existance état initial
     a.transition_valide()  # Transition valide  
     a.mot_commence_0()  # Tout mot commence à 0
-    a.lien_chemin_transition()  # Lien entre chemin et transition
     a.chemin_implique_transition()   # Chemin implique transition
     a.transition_implique_chemin()  # Transition implique chemin
     a.un_chemin_possible()  # Un seul chemin possible
@@ -353,30 +378,28 @@ def gen_autcard(alphabet: str, pos: list[str], neg: list[str], k: int, ell: int)
 
 # Q7
 def gen_autn(alphabet: str, pos: list[str], neg: list[str], k: int) -> NFA:
-    a = Automate(alphabet, pos, neg, k)
+    a = AutomateNFA(alphabet, pos, neg, k)
     a.existance_etat_initial()  # existance état initial
     a.mot_commence_0()  # Tout mot commence à 0
     a.transition_valide()
-    a.un_chemin_possible()
-    a.lien_chemin_transition()  # Lien entre chemin et transition
-    a.chemin_implique_transition()   # Chemin implique transition
+    a.existe_1_chemin_mots_acceptants()
     a.transition_implique_chemin()  # Transition implique chemin
-    a.au_moins_une_exec_acceptante()
     a.exec_finie_etat_non_acceptant()  # Exécution finie sur état non-acceptant
+    a.lien_etat_final_transition()
+    a.etat_final_implique_etat_acceptant()
+    a.au_moins_un_etat_acceptant()
+    a.lien_chemin_transition()
     resultat = a.solve_aut() 
     return a.create_nfa(resultat)
 
 def main():
-    #test_aut()
-    #test_minaut()
-    #test_autc()
-    #test_autr()
-    #test_autcard()
+    test_aut()
+    test_minaut()
+    test_autc()
+    test_autr()
+    test_autcard()
     test_autn()
 
 if __name__ == '__main__':
     main()
-        # cnf = CNF()  # construction d'un objet formule en forme normale conjonctive (Conjunctive Normal Form)
-        # print(elem)
-        # gen_aut(elem[0],elem[1],elem[2],elem[3])
 
